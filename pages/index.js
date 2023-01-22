2485c3a82ac2bf4c50bd8d9b3a28d6c3894725b6
Mridul2820 <immridulpanda2820@gmail.com>
2023-01-22 15:12:18 +0530

    Generate PDF Done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,55 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [files, setFiles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const getFiles = async () => {
+    const res = await fetch("/api/files");
+    const files = await res.json();
+
+    setFiles(files);
+  };
+
+  useEffect(() => {
+    getFiles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const downloadResourcesOnClick = async () => {
+    setLoading(true);
+    try {
+      const zip = new JSZip();
+      const remoteZips = files.map(async (file) => {
+        const response = await fetch(file.url);
+        const data = await response.blob();
+        zip.file(`${file.name}.${file.type}`, data);
+
+        return data;
+      });
+
+      Promise.all(remoteZips)
+        .then(() => {
+          zip.generateAsync({ type: "blob" }).then((content) => {
+            saveAs(content, "zip-download-next-js.zip");
+          });
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,7 +66,13 @@ export default function Home() {
           Generate ZIP with file links in Next JS
         </h1>
 
-        <button className={styles.button}>Download The Zip</button>
+        <button
+          onClick={downloadResourcesOnClick}
+          className={styles.button}
+          disabled={loading}
+        >
+          Download The Zip
+        </button>
       </main>
     </div>
   );
